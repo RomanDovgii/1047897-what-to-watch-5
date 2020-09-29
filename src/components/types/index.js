8f4a0cd8583bbe1338c9847c3d76c0eb7747298e
RomanDovgii <romandovgiiwork@gmail.com>
2020-09-29 19:42:11 +0300

    removed shape

--- src/components/types/index.js
+++ src/components/types/index.js
@@ -1,36 +1,36 @@
-import {PropTypes, shape} from "prop-types";
+import PropTypes from "prop-types";
 
-export const appType = shape({
+export const appType = {
   name: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   releaseDate: PropTypes.number.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   movies: PropTypes.arrayOf(PropTypes.string).isRequired
-});
+};
 
-export const mainPageType = shape({
+export const mainPageType = {
   name: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   releaseDate: PropTypes.number.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   movies: PropTypes.arrayOf(PropTypes.string).isRequired
-});
+};
 
 export const movieListItemType = PropTypes.string.isRequired;
 
 export const movieListType = PropTypes.arrayOf(movieListItemType).isRequired;
 
-export const movieCardTopType = shape({
+export const movieCardTopType = {
   name: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   releaseDate: PropTypes.number.isRequired
-});
+};
 
 export const genresListItemType = PropTypes.string.isRequired;
 
 export const genresListType = PropTypes.arrayOf(genresListItemType).isRequired;
 
-export const catalogType = shape({
+export const catalogType = {
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   movies: PropTypes.arrayOf(PropTypes.string).isRequired
-});
+};
