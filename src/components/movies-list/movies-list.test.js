b23e5b8b9cb4f85fee5f586586a01b2ce242c2c2
Roman Dovgii <romandovgiiwork@gmail.com>
2020-11-18 01:51:53 +0300

    Added test for movies-list. It doesn't need e2e test so I move on.

---
+++ src/components/movies-list/movies-list.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ShallowRenderer from "react-test-renderer/shallow";
+import MoviesList from "./movies-list";
+
+const testMovie = {
+  id: 1,
+  name: `The Grand Budapest Hotel`,
+  posterImage: `img/the-grand-budapest-hotel-poster.jpg`,
+  previewImage: `img/the-grand-budapest-hotel.jpg`,
+  backgroundImage: `img/the-grand-budapest-hotel-bg.jpg`,
+  backgroundColor: `#ffffff`,
+  videoLink: `https://some-link`,
+  previewVideoLink: `https://some-link`,
+  description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
+  rating: 8.9,
+  scoresCount: 240,
+  director: `Wes Andreson`,
+  starring: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`, `Saoirse Ronan`],
+  runTime: 99,
+  genre: `Comedy`,
+  released: 2014,
+  isFavorite: false
+};
+
+const testMovies = [
+  testMovie,
+  testMovie,
+  testMovie
+];
+
+it(
+    `Should render correct movie list`,
+    () => {
+      const renderer = new ShallowRenderer();
+
+      const tree = renderer
+      .render(
+          <MoviesList
+            movies={testMovies}
+          >
+            <React.Fragment/>
+          </MoviesList>
+      );
+
+      expect(tree).toMatchSnapshot();
+    }
+);
