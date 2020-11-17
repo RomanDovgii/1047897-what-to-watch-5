79416dc72c9a209a86081d439fa79f93551c44a7
RomanDovgii <romandovgiiwork@gmail.com>
2020-11-17 20:59:23 +0300

    fixed bugs in small movie card

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import {smallMovieCardType} from "../types/types";
 import {CallSource} from "../../utils/const";
 import {connect} from "react-redux";
-import {selectMovie} from "../../store/actions/action";
+import {selectMovie, redirectToRoute} from "../../store/actions/action";
 import {fetchSelectedMovieComments} from "../../store/actions/api-actions";
 
 const SmallMovieCard = (props) => {
   const {movie, renderPlayer, isPlaying, onClick, onMouseEnter, onMouseLeave} = props;
-  const {name, previewVideoLink, previewImage, id} = movie;
+  const {name, previewVideoLink, previewImage} = movie;
 
   return (
     <article
       className="small-movie-card catalog__movies-card"
       onMouseEnter = {onMouseEnter}
       onMouseLeave = {onMouseLeave}
-      onClick = {(evt) => {
-        evt.preventDefault();
-
-        onClick(movie);
-      }}>
+    >
       <div className="small-movie-card__image">
         {renderPlayer(previewVideoLink, previewImage, CallSource.CATALOG, isPlaying)}
       </div>
       <h3 className="small-movie-card__title">
-        <Link
+        <a
           className="small-movie-card__link"
-          to={`/films/${id}`}
+          href="#"
+          onClick = {(evt) => {
+            evt.preventDefault();
+
+            onClick(movie);
+          }}
         >
           {name}
-        </Link>
+        </a>
       </h3>
     </article>
   );
@@ -41,6 +41,7 @@ const mapDispatchToProps = (dispatch) => ({
   onClick(movie) {
     dispatch(selectMovie(movie));
     dispatch(fetchSelectedMovieComments(movie.id));
+    dispatch(redirectToRoute(`/films/${movie.id}`));
   }
 });
 
