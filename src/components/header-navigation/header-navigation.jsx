c363f5b1e24502a1f1b59d2e9c41a36b212efea4
RomanDovgii <romandovgiiwork@gmail.com>
2020-11-18 11:49:56 +0300

    covered all components with tests

--- src/components/header-navigation/header-navigation.jsx
+++ src/components/header-navigation/header-navigation.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import {connect} from "react-redux";
+import {redirectToRoute} from "../../store/actions/action";
 import {isRenderedType} from "../types/types";
 
 const HeaderNavigation = (props) => {
-  const {isRendered, movie} = props;
+  const {isRendered, onMovieLinkClick, movie} = props;
 
   if (isRendered) {
     return (
       <nav className="breadcrumbs">
         <ul className="breadcrumbs__list">
           <li className="breadcrumbs__item">
-            <a href="movie-page.html" className="breadcrumbs__link">{movie.name}</a>
+            <a
+              href="#"
+              className="breadcrumbs__link"
+              onClick={(evt) => {
+                evt.preventDefault();
+
+                onMovieLinkClick(movie);
+              }}
+            >{movie.name}</a>
           </li>
           <li className="breadcrumbs__item">
             <a className="breadcrumbs__link">Add review</a>
@@ -25,9 +34,15 @@ const HeaderNavigation = (props) => {
 
 HeaderNavigation.propTypes = isRenderedType;
 
+const mapDispatchToProps = (dispatch) => ({
+  onMovieLinkClick(movie) {
+    dispatch(redirectToRoute(`/films/${movie.id}`));
+  }
+});
+
 const mapStateToProps = ({DATA}) => ({
   movie: DATA.selectedMovie
 });
 
 export {HeaderNavigation};
-export default connect(mapStateToProps)(HeaderNavigation);
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderNavigation);
---
+++ src/components/header-navigation/header-navigation.test.js
@@ -0,0 +1,58 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {noop} from "../../utils/utils";
+import {HeaderNavigation} from "./header-navigation";
+
+const testMovie = {
+  id: 1,
+  name: `The Grand Budapest Hotel`,
+  posterImage: `img/the-grand-budapest-hotel-poster.jpg`,
+  previewImage: `img/the-grand-budapest-hotel.jpg`,
+  backgroundImage: `img/the-grand-budapest-hotel-bg.jpg`,
+  backgroundColor: `#ffffff`,
+  videoLink: `https://some-link`,
+  previewVideoLink: `https://some-link`,
+  description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
+  rating: 8.9,
+  scoresCount: 240,
+  director: `Wes Andreson`,
+  starring: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`, `Saoirse Ronan`],
+  runTime: 99,
+  genre: `Comedy`,
+  released: 2014,
+  isFavorite: false
+};
+
+it(
+    `Should render correct HeaderNavigation`,
+    () => {
+      const tree = renderer
+      .create(
+          <HeaderNavigation
+            isRendered={true}
+            onMovieLinkClick={noop}
+            movie={testMovie}
+          />
+      )
+      .toJSON();
+
+      expect(tree).toMatchSnapshot();
+    }
+);
+
+it(
+    `Should render nothing`,
+    () => {
+      const tree = renderer
+      .create(
+          <HeaderNavigation
+            isRendered={false}
+            onMovieLinkClick={noop}
+            movie={testMovie}
+          />
+      )
+      .toJSON();
+
+      expect(tree).toMatchSnapshot();
+    }
+);
